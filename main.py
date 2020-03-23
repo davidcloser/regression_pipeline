4edb915d1bcdc648f026c11836d9c6da8c5e3433
david.marques <david.marques@closer.pt>
2020-03-23 21:30:40 +0000

    Finished Pipeline

--- main.py
+++ main.py
@@ -8,10 +8,18 @@ from sklearn.svm import SVR
 from sklearn.ensemble import AdaBoostRegressor, RandomForestRegressor
 import seaborn as sns
 import matplotlib.pyplot as plt
-from tpot import TPOTRegressor
 
 label = "label"
 data = pd.read_csv("data/data.csv")
+
+# Split the scores
+# data[label] = data[label].shift(-1)
+# data.dropna(inplace=True)
+X = data.drop(label, axis=1)
+y = data[label]
+
+# Scale X
+X = StandardScaler().fit_transform(X)
 
 # Visualizing the correlation
 sns.heatmap(data=data.corr(), annot=True)
@@ -20,28 +28,21 @@ plt.savefig("output/correlation_matrix.png")
 plt.show()
 
 # Get the most correlated columns
-correlation = SelectKBest(score_func=f_regression, k="all").fit(data.drop(label, axis=1), data[label])
+correlation = SelectKBest(score_func=f_regression, k="all").fit(X, y)
 correlation_df = pd.DataFrame()
 correlation_df["columns"] = data.drop(label, axis=1).columns.values
 correlation_df["scores"] = correlation.scores_
 correlation_df["pvalues"] = correlation.pvalues_
 correlation_df.sort_values(by="scores", ascending=False, inplace=True)
-correlation_df.to_csv("output/correlation_df.csv")
+correlation_df.to_csv("output/correlation_df.csv", index=False, sep=";")
 
-# Split the scores
-data[label] = data[label].shift(-1)
-data.dropna(inplace=True)
-X = data.drop(label, axis=1)
-y = data[label]
-
-# Scale X
-X = StandardScaler().fit_transform(X)
 
 # Define the model Dictionaries
+alphas =  np.arange(0.2, 5, 0.1)
 models = {
-    "Lasso": (Lasso, {"alpha": np.arange(0.5, 2.5, 0.5)}),
-    "AdaBoost": (AdaBoostRegressor, {"learning_rate": np.arange(0.5, 2, 0.5), "loss": ["linear", "square", "exponential"]}),
-    "RandomForest": (RandomForestRegressor, {"n_estimators": np.arange(10, 100, 10,)}),
+    "Lasso": (Lasso, {"alpha": alphas}),
+    "AdaBoost": (AdaBoostRegressor, {"learning_rate": alphas, "loss": ["linear", "square", "exponential"]}),
+    "RandomForest": (RandomForestRegressor, {"n_estimators": np.arange(10, 500, 10,)}),
     "SVR": (SVR, {"kernel": ["linear", "rbf", "sigmoid"]})
 }
 
@@ -54,6 +55,5 @@ for model_name, model in models.items():
     r2_scores[model_name] = {"r2": reg.best_score_, "params": reg.best_params_}
 
 all_scores = pd.DataFrame(r2_scores).T.sort_values(by="r2", ascending=False)
+all_scores.to_csv("output/all_scores.csv", index=False, sep=";")
 print(all_scores)
-
-
